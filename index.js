e41128de7c544fef75322be3bac12eec6e7c6500
ilmoheiska <ilmo.heiska@gmail.com>
2019-09-14 16:59:54 +0300

    to heroku

--- index.js
+++ index.js
@@ -146,6 +146,6 @@ const generateId = () => {
 }
 
 const PORT = process.env.PORT || 3001
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
 })
